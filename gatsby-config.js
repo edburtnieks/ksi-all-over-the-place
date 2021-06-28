05d87b7436e9895c23bc6b66bfcc29663092ffad
Edgar Burtnieks <edburtnieks@pm.me>
2021-06-29 01:00:57 +0300

    Add headers for FF

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,14 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: "gatsby-plugin-netlify",
+      options: {
+        allPageHeaders: [
+          "Cross-Origin-Opener-Policy: same-origin",
+          "Cross-Origin-Embedder-Policy: require-corp",
+        ],
+      },
+    },
   ],
 }
